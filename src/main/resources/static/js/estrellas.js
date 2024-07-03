defdd4ce5eacc678ac6bdd79854409cec7e87b50
EzequielMovia <136653654+EzequielMovia@users.noreply.github.com>
2024-07-02 21:55:45 -0300

    actualizacion fron y back

--- src/main/resources/static/js/estrellas.js
+++ src/main/resources/static/js/estrellas.js
@@ -1,12 +1,26 @@
-const stars = document.querySelectorAll('.star');
+document.querySelectorAll('.rating').forEach(rating => {
+    const stars = rating.querySelectorAll('.star');
+    const averageDisplay = rating.nextElementSibling.querySelector('.average-value');
+    let totalRatings = 0;
+    let numRatings = 0;
 
-stars.forEach(function(star, index) {
-    star.addEventListener('click', function () {
-        for (let i=0; i<=index; i++) {
-            stars[i].classList.add('checked');
-        }
-        for (let i=index+1; i<stars.length; i++) {
-            stars[i].classList.remove('checked');
-        }
-    })
-})
+    stars.forEach(function(star, index) {
+        star.addEventListener('click', function () {
+            let ratingValue = stars.length - index;
+            totalRatings += ratingValue;
+            numRatings += 1;
+
+            // Update stars display
+            for (let i = 0; i <= index; i++) {
+                stars[i].classList.add('checked');
+            }
+            for (let i = index + 1; i < stars.length; i++) {
+                stars[i].classList.remove('checked');
+            }
+
+            // Calculate and display average rating
+            const averageRating = totalRatings / numRatings;
+            averageDisplay.textContent = averageRating.toFixed(1);
+        });
+    });
+});
